098b789e0313d6584afa31eaec219aaf34d08cf7
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-03 19:26:42 -0600

    edit to idea model test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -8,4 +8,11 @@ describe Idea, type: :model do
     should validate_inclusion_of(:quality).
       in_array(['genius', 'plausible', 'swill'])
   end
+  
+  it "orders ideas by the created date" do
+    old_idea = Idea.create(title: "Old Idea", body: "old idea description")
+    new_idea = Idea.create(title: "New Idea", body: "new idea description")
+
+    expect(Idea.order_by_created_date.last).to eq(old_idea)
+  end
 end
