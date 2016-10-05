295004fd858761ca7f517d992f981d1266c1ce9d
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-04 19:13:20 -0600

    edit to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
-require "application_responder"
-
 class ApplicationController < ActionController::Base
-  self.responder = ApplicationResponder
-  respond_to :html
-
   protect_from_forgery with: :null_session
 end
